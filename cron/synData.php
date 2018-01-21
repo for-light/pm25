fa4bf9f8714d8dd8207a1ea8996feb149be915a9
lutiansong <new.gooltsgle@gmail.com>
2018-01-21 15:57:44 +0800

    reset

--- cron/synData.php
+++ cron/synData.php
@@ -10,48 +10,40 @@
 $dsn = "mysql:host=localhost;dbname=pm25";
 $db = new PDO($dsn, 'root', 'root');
 $db->query('SET NAMES utf8');
-$day = date('Y-m-d');
-$hour = date('H');
-//$day = '2018-01-16';
-//$hour = '16';
-$sql = "select id from data where day = '{$day}' and hour = '{$hour}' limit 1";
-$result = $db->query($sql)->fetch(PDO::FETCH_ASSOC);
-if (!$result) {
-    $host = "https://ali-pm25.showapi.com";
-    $path = "/pm25-top";
-    $method = "GET";
-    $appcode = "50877461394c49ea89733c51498af352";
-    $headers = array();
-    array_push($headers, "Authorization:APPCODE " . $appcode);
-    $url = $host . $path;
 
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_FAILONERROR, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    if (1 == strpos("$".$host, "https://"))
-    {
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    }
-    $content = json_decode(curl_exec($curl), true);
-    $data = array();
-    $sql = '';
+$host = "https://ali-pm25.showapi.com";
+$path = "/pm25-top";
+$method = "GET";
+$appcode = "50877461394c49ea89733c51498af352";
+$headers = array();
+array_push($headers, "Authorization:APPCODE " . $appcode);
+$url = $host . $path;
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_FAILONERROR, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+//if (1 == strpos("$".$host, "https://"))
+//{
+//    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+//}
+$content = json_decode(curl_exec($curl), true);
+$data = array();
+$sql = '';
 //var_dump($content);exit();
-    foreach ($content['showapi_res_body']['list'] as $row) {
-        $data = array(
-            'day' => $day,
-            'hour' => $hour,
-            'area' => $row['area'],
-            'pm25' => is_numeric($row['pm2_5']) ? $row['pm2_5'] : 0,
-            'aqi' => is_numeric($row['aqi']) ? $row['aqi'] : 0,
-        );
-        $sql .= "insert into data(day, hour, area, pm25, aqi, created)values('{$data['day']}', {$data['hour']}, '{$data['area']}', {$data['pm25']}, {$data['aqi']}, now());";
-    }
-    $db->exec($sql) or die(print_r($day . $hour . 'error' . PHP_EOL));
-    echo $day . $hour . 'complete!' . PHP_EOL;
+foreach ($content['showapi_res_body']['list']  as $row) {
+    $data = array(
+        'day' => date('Y-m-d'),
+        'hour' => date('H'),
+        'area' => $row['area'],
+        'pm25' => is_numeric($row['pm2_5']) ? $row['pm2_5'] : 0,
+        'aqi' => is_numeric($row['aqi']) ? $row['aqi'] : 0,
+    );
+    $sql .= "insert into data(day, hour, area, pm25, aqi, created)values('{$data['day']}', {$data['hour']}, '{$data['area']}', {$data['pm25']}, {$data['aqi']}, now());";
 }
-
+$count = $db->exec($sql) or die(print_r($db->errorInfo(), true));
+echo date('Y-m-d H:i:s') . PHP_EOL;
